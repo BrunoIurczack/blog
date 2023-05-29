09ef896c442c798761335692d68a958067347596
Bruno Felipe <bruno@matheuss-MacBook-Air-2.local>
2023-05-29 17:07:19 -0300

    Crud 3

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -15,10 +15,10 @@ module Admin
     def edit; end
 
     def create
-      @post = Post.new(customer_params)
+      @post = Post.new(post_params)
 
       if @post.save
-        redirect_to post_url(@post), notice: "Post was successfully created."  
+        redirect_to admin_post_url(@post), notice: "Post was successfully created."  
       else
         render :new, status: :unprocessable_entity
       end
@@ -26,7 +26,7 @@ module Admin
 
     def update
       if @post.update(post_params)
-        redirect_to post_url(@post), notice: "Post was successfully updated."
+        redirect_to admin_post_url(@post), notice: "Post was successfully updated."
       else
         render :edit, status: :unprocessable_entity
       end
@@ -34,7 +34,7 @@ module Admin
 
     def destroy
       @post.destroy
-      redirect_to posts_path
+      redirect_to admin_posts_path
     end
 
     private
@@ -42,7 +42,7 @@ module Admin
         @post = Post.find(params[:id])
       end
 
-      def customer_params
+      def post_params
         params.require(:post).permit(:title, :resume, :publication_date, :content)
       end
   end
