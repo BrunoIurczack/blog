94b31b94fc2363694571c92b86dd688485882061
Bruno Felipe <bruno@matheuss-MacBook-Air-2.local>
2023-06-26 11:06:52 -0300

    Create post main e second

--- app/controllers/site/posts_controller.rb
+++ app/controllers/site/posts_controller.rb
@@ -1,7 +1,17 @@
 module Site
   class PostsController < ApplicationController
     def index
-      @pagy, @records = pagy(Post.all)
+      excluded_ids = []
+
+      #@main = Post.where(highlight: :main).order(publication_date: :desc).first
+      @main = Post.main.published.first
+      excluded_ids << @main.id
+      @secondary = Post.where(highlight: :secondary).order(publication_date: :desc).take(2)
+      excluded_ids << @secondary.map(&:id).flatten
+      # flatten
+      # [1, [2, 3]].flatten => [1, 2, 3]
+      #               @secondary.map { |a| a.id }
+      @pagy, @records = pagy(Post.where.not(id: excluded_ids))
     end
 
     def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :category, optional: true
   enum highlight: { main: 0, secondary: 1 }
+
+  scope :main, -> { where(highlight: :main).order(publication_date: :desc) }
+  scope :published, -> { where("publication_date <= ?", Time.now) }
 end
